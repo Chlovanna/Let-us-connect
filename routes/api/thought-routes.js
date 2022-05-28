87e03d1bc00c771a2b746b5b0351172c20f3ad1c
Chlovanna <ashdmt39@gmail.com>
2022-05-27 23:16:39 -0400

    create thought route

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    removeThought,
+    addReaction,
+    removeReaction
+  } = require('../../controllers/thought-controller');
+
+  router
+  .route('/')
+  .get(getAllThoughts)
+  .post(addThought);
+
+  router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(removeThought);
+
+  router
+  .route('/:thoughtId/reactions')
+  .post(addReaction)
+
+  router
+  .route('/:thoughtId/:reactionId')
+  .delete(removeReaction)
+
+module.exports = router; 
